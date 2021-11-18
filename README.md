28cdd1e22df92a90a06294ed3f46f35135eddc5e
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-11-18 16:48:46 +0900

    react 화면 구성

--- README.md
+++ README.md
@@ -11,5 +11,22 @@
 ### React
 
 - yarn add react-router-dom
-- yarn add redux react-redux
+<!-- - yarn add redux react-redux -->
+- yarn add react-router-dom@5
 - yarn add react-bootstrap bootstrap
+
+```txt
+import 'bootstrap/dist/css/bootstrap.min.css';
+```
+
+### 프로젝트 세팅
+
+```json
+{
+  "singleQuote": true,
+  "semi": true,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "printWidth": 80
+}
+```
--- book-frontend/src/App.js
+++ book-frontend/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container } from 'react-bootstrap';
+import { Route } from 'react-router-dom';
+import Header from './components/Header';
+import SaveForm from './pages/book/SaveForm';
+import Detail from './pages/book/Detail';
+import LoginForm from './pages/user/LoginForm';
+import JoinForm from './pages/user/JoinForm';
+import UpdateForm from './pages/book/UpdateForm';
+import Home from './pages/book/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Container>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/saveForm" exact={true} component={SaveForm} />
+        <Route path="/book/:id" exact={true} component={Detail} />
+        <Route path="/loginForm" exact={true} component={LoginForm} />
+        <Route path="/joinForm" exact={true} component={JoinForm} />
+        <Route path="/updateForm/:id" exact={true} component={UpdateForm} />
+      </Container>
     </div>
   );
 }
